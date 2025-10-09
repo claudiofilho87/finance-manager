2385d5a7f3dcaba795b6eb7d6b07405d8f9f3423
Claudio Filho <claudiofilho1525@gmail.com>
2025-10-09 15:40:18 -0300

    refactor: include bill value cents in schedule command to bill ocorrence generate

--- FinanceManager/Services/BillOcorrenceGeneratorService.cs
+++ FinanceManager/Services/BillOcorrenceGeneratorService.cs
@@ -64,6 +64,7 @@ public class BillOcorrenceGeneratorService : BackgroundService
                     BillId = bill.Id,
                     Date = newDate,
                     Status = BillStatus.Pending,
+                    ValueCents = bill.ValueCents,
                     CreatedAt = now,
                     UpdatedAt = now
                 });
